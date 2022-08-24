7caed8a76b33a82a84d8159b190797dc05088238
YungFan <yangfanvw@qq.com>
2022-08-24 16:07:58 +0800

    iOS新特性案例

---
+++ iOS 14/UICollectionViewBuildList-自定义Cell/UICollectionViewBuildList/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  UICollectionViewBuildList
+//
+//  Created by 杨帆 on 2020/7/2.
+//
+
+import Foundation
+
+
+struct City: Hashable {
+    let name: String
+    let identifier = UUID()
+}
